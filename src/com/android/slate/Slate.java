ae594f0b6530c20d8a21de606583498ce8e81e49
Daniel Sandler <dsandler@dsandler-macbookpro.local>
2010-12-27 21:57:41 -0600

    initial version

---
+++ src/com/android/slate/Slate.java
@@ -0,0 +1,225 @@
+package com.android.slate;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class Slate extends View {
+    private static final boolean DEBUG = true;
+    private static final String TAG = "Slate";
+
+    private static final boolean DEBUG_INVALIDATE_ALL = false;
+    private static final boolean DEBUG_CURVES = false;
+
+    private static final boolean BEZIER = true;
+
+    private static final float INVALIDATE_PADDING = 4.0f;
+
+    private float mPressureVariation = 0.0f;
+    private float mPressureExponent = 1.0f;
+
+    private float mSizeVariation = 1.0f;
+    private float mSizeExponent = 2.0f;
+
+    private float mRadius = 8.0f;
+
+    private static final float TOUCH_SIZE_RANGE_MIN = 1.0f;
+    private static final float TOUCH_SIZE_RANGE_MAX = 50.0f;
+
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private final RectF mRect = new RectF();
+    private final Paint mPaint, mStrokePaint;
+    private final Paint mDebugPaints[] = new Paint[2];
+    private float mLastX = 0, mLastY = 0, mLastR = -1;
+    private float mTanX = 0, mTanY = 0;
+
+    public Slate(Context c, AttributeSet as) {
+        super(c);
+        setFocusable(true);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setARGB(255, 255, 255, 255);
+//        mPaint.setARGB(255, 0, 255, 0);
+        mStrokePaint = new Paint(mPaint);
+        mStrokePaint.setStyle(Paint.Style.STROKE);
+        mStrokePaint.setStrokeCap(Paint.Cap.ROUND);
+
+        if (DEBUG_CURVES) {
+            mStrokePaint.setARGB(128, 255,255,255);
+            mPaint.setARGB(128, 255,255,255);
+            mDebugPaints[0] = new Paint();
+            mDebugPaints[0].setStyle(Paint.Style.STROKE);
+            mDebugPaints[0].setStrokeWidth(2.0f);
+            mDebugPaints[0].setARGB(255, 0, 255, 255);
+            mDebugPaints[1] = new Paint(mDebugPaints[0]);
+            mDebugPaints[1].setARGB(255, 0, 0, 255);
+        }
+    }
+
+    public void clear() {
+        if (mCanvas != null) {
+            mPaint.setARGB(0xff, 0, 0, 0);
+            mCanvas.drawPaint(mPaint);
+            invalidate();
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw,
+            int oldh) {
+        int curW = mBitmap != null ? mBitmap.getWidth() : 0;
+        int curH = mBitmap != null ? mBitmap.getHeight() : 0;
+        if (curW >= w && curH >= h) {
+            return;
+        }
+
+        w = getWidth();
+        h = getHeight();
+
+        if (curW < w) curW = w;
+        if (curH < h) curH = h;
+
+        Bitmap newBitmap = Bitmap.createBitmap(curW, curH,
+                Bitmap.Config.RGB_565);
+        Log.d(TAG, "created bitmap " + curW + "x" + curH);
+        Canvas newCanvas = new Canvas();
+        newCanvas.setBitmap(newBitmap);
+        if (mBitmap != null) {
+            newCanvas.drawBitmap(mBitmap, 0, 0, null);
+        }
+        mBitmap = newBitmap;
+        mCanvas = newCanvas;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mBitmap != null) {
+            canvas.drawBitmap(mBitmap, 0, 0, null);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x, y;
+        int action = event.getActionMasked();
+        if (action != MotionEvent.ACTION_CANCEL) {
+            int N = event.getHistorySize();
+            int P = 1; // event.getPointerCount();
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < P; j++) {
+                    x = event.getHistoricalX(j, i);
+                    y = event.getHistoricalY(j, i);
+                    drawPoint(x, y,
+                            event.getHistoricalPressure(j, i),
+                            event.getHistoricalTouchMajor(j, i));
+                }
+            }
+            for (int j = 0; j < P; j++) {
+                x = event.getX(j);
+                y = event.getY(j);
+                drawPoint(x, y, event.getPressure(j), event.getTouchMajor(j));
+            }
+        }
+
+        if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_CANCEL) {
+            // reset
+            mLastX = mLastY = mTanX = mTanY = 0;
+            
+            mLastR = -1;
+        }
+        return true;
+    }
+
+    private void drawPoint(float x, float y, float pressure, float width) {
+//        Log.i("TouchPaint", "Drawing: " + x + "x" + y + " p="
+//                + pressure + " width=" + width);
+        
+
+        if (width < TOUCH_SIZE_RANGE_MIN) width = TOUCH_SIZE_RANGE_MIN;
+        else if (width > TOUCH_SIZE_RANGE_MAX) width = TOUCH_SIZE_RANGE_MAX;
+        float widthNorm = (width - TOUCH_SIZE_RANGE_MIN)
+            / (TOUCH_SIZE_RANGE_MAX - TOUCH_SIZE_RANGE_MIN);
+
+        // TODO: pressure
+        float pressureNorm = pressure;
+
+        float r = mRadius * (1
+                + (float) (Math.pow(widthNorm, mSizeExponent) - 0.5f) * mSizeVariation
+                + (float) (Math.pow(pressureNorm, mPressureExponent) - 0.5f) * mPressureVariation
+            );
+        Log.d(TAG, String.format("(%g, %g): wnorm=%g rad=%g", x, y, widthNorm, r));
+
+        if (mBitmap != null) {
+            mCanvas.drawCircle(x, y, r, mPaint);
+
+            mRect.set((x - r - INVALIDATE_PADDING),
+                      (y - r - INVALIDATE_PADDING),
+                      (x + r + INVALIDATE_PADDING),
+                      (y + r + INVALIDATE_PADDING));
+
+            if (mLastR >= 0) {
+                // connect the dots, la-la-la
+                // TODO: use a trapezoid from circle tangents
+                mStrokePaint.setStrokeWidth(r+mLastR); // average x 2
+//                mCanvas.drawLine(mLastX, mLastY, x, y, mStrokePaint);
+                
+                Path p = new Path();
+                p.moveTo(mLastX, mLastY);
+
+                float controlX = mLastX + mTanX*0.5f;
+                float controlY = mLastY + mTanY*0.5f;
+
+                if (BEZIER && (mTanX != 0 || mTanY != 0)) {
+                    p.quadTo(controlX, controlY, x, y);
+                } else {
+                    p.lineTo(x, y);
+                }
+                mCanvas.drawPath(p, mStrokePaint);
+    
+                mRect.union((mLastX - mLastR - INVALIDATE_PADDING),
+                            (mLastY - mLastR - INVALIDATE_PADDING));
+                mRect.union((mLastX + mLastR + INVALIDATE_PADDING),
+                            (mLastY + mLastR + INVALIDATE_PADDING));
+
+                mTanX = x - controlX;
+                mTanY = y - controlY;
+
+                if (DEBUG_CURVES) {
+//                    mCanvas.drawLine(mLastX, mLastY, controlX, controlY, mDebugPaints[0]);
+//                    mCanvas.drawLine(controlX, controlY, x, y, mDebugPaints[0]);
+                    mCanvas.drawPoint(controlX, controlY, mDebugPaints[0]);
+                    mCanvas.drawLine(mLastX, mLastY, x, y, mDebugPaints[1]);
+                }
+            }
+
+            if (DEBUG_INVALIDATE_ALL) {
+                invalidate();
+            } else {
+                Rect dirty = new Rect();
+                mRect.roundOut(dirty);
+                invalidate(dirty);
+            }
+        }
+
+        mLastX = x;
+        mLastY = y;
+        mLastR = r;
+    }
+}
+
---
+++ src/com/android/slate/SlateActivity.java
@@ -0,0 +1,15 @@
+package com.android.slate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SlateActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
